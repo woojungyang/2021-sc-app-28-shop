5e7ab2dc00319bf1801dcb8f07463268ce6e6f8d
woojungyang <blossom1113@nate.com>
2021-12-13 15:12:41 +0900

    deploy setting

--- modules/dotenv-init.js
+++ modules/dotenv-init.js
@@ -1,8 +1,12 @@
-const path = require('path')
-const dotenv = require('dotenv')
+const path = require('path');
+const dotenv = require('dotenv');
 module.exports = () => {
-  let { NODE_ENV } = process.env
+  let { NODE_ENV } = process.env;
   dotenv.config({
-    path: path.join(__dirname, '../', NODE_ENV === 'production' ? '.env.pro' : '.env.dev')
-  })
-}
+    path: path.join(
+      __dirname,
+      '../',
+      NODE_ENV === 'production' ? 'production' : 'development'
+    ),
+  });
+};
