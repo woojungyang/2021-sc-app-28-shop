350854260ca84f94ebf7835d3d2cdd876c408066
woojungyang <blossom1113@nate.com>
2021-12-14 13:16:45 +0900

    ...

--- seeders3/20211105017200-demo-color.js
+++ seeders3/20211105017200-demo-color.js
@@ -1,42 +1,20 @@
+const fs = require('fs-extra');
+const path = require('path');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const insertColor = [];
-    const name = [
-      'Light Grey',
-      'Indigo Blue',
-      'Pink',
-      'Yellow Green',
-      'Dark Grey',
-      'Black',
-      'White',
-      'Light Cyan',
-      'Orange',
-      'Red',
-      'Blue',
-      'Green',
-    ];
-    const code = [
-      '#808080',
-      '#064384',
-      '#cc0fa3',
-      '#cde232',
-      '#333333',
-      '#000000',
-      '#e6e6e6',
-      '#35d5fd',
-      '#f35d2b',
-      '#dc0909',
-      '#003ac2',
-      '#1ba300',
-    ];
-    for (let i = 0; i < name.length; i++)
+    const color = fs.readJsonSync(path.join(__dirname, '../json/color.json'));
+    var code = Object.keys(color);
+    var name = Object.values(color);
+    for (let i = 1; i <= 50; i++) {
       insertColor.push({
+        id: i,
         name: name[i],
         code: code[i],
         createdAt: new Date(),
         updatedAt: new Date(),
       });
-
+    }
     await queryInterface.bulkInsert('color', insertColor);
   },
 
